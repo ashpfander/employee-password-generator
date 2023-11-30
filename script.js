ba7f5be5dc0b5fe0d3d034c9e778bdbc05be2bd9
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.attlocal.net>
2023-11-30 14:34:49 -0500

    added character length prompt

--- script.js
+++ script.js
@@ -9,19 +9,24 @@ var specialCharacters = "~!@#$%^&*()_+?-=.";
 
 // Write password to the #password input
 var writePassword = function() {
+
+  // Starts with first prompt of character length selection and if/else if statements
+  var characterLength = window.prompt("Between 8 and 128, how many characters would you like your password to be?");
+  if (characterLength < 8 || characterLength > 128) {
+    window.alert("Please enter a valid number.");
+    writePassword();
+  }
+
+  var upperOptions = window.confirm("Include uppercase letters?\nClick OK if yes.");
+  var lowerOptions = window.confirm("Include lowercase letters?\nClick OK if yes.");
+  var numberOptions = window.confirm("Include numbers?\nClick OK if yes.");
+  var specialOptions = window.confirm("Include special characters?\nClick OK if yes.");
+  var showPassword = window.alert("Here's your password!");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  // Added all prompts
-  // Needs if instances
-  var characterLength = window.prompt("Between 8 and 128, how many characters would you like your password to be?");
-  var upperOptions = window.alert("Include uppercase letters?\nClick OK if yes.");
-  var lowerOptions = window.alert("Include lowercase letters?\nClick OK if yes.");
-  var numberOptions = window.alert("Include numbers?\nClick OK if yes.");
-  var specialOptions = window.alert("Include special characters?\nClick OK if yes.");
-  var showPassword = window.alert("Here's your password!");
 }
 
 // Add event listener to generate button
