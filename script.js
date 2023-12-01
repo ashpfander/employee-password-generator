866a891eb194beefbc33ebee32de30913f180c87
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2023-12-01 16:28:17 -0500

    created randomizing of confirmed criterias

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 // Different password criterias
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "1234567890";
-var specialCharacters = "~!@#$%^&*()_+?-=.";
+// Split for the code to read each character individually within the strings
+var criteria = {
+  uppercaseLetters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+  lowercaseLetters: "abcdefghijklmnopqrstuvwxyz".split(""),
+  numbers: "1234567890".split(""),
+  specialCharacters: "~!@#$%^&*()_+?-=.".split("")
+};
 
 // Write password to the #password input
 var writePassword = function() {
@@ -17,13 +20,33 @@ var writePassword = function() {
     writePassword();
   }
 
+  // If confirmed, randomize the specific criteria for uppercase, lowercase, numbers, and special characters
   var upperOptions = window.confirm("Include uppercase letters?\nClick OK if yes.");
+  if (upperOptions) {
+    var random1 = Math.floor(Math.random() * criteria.uppercaseLetters.length);
+    var randomUpper = criteria.uppercaseLetters[random1];
+  }
+
   var lowerOptions = window.confirm("Include lowercase letters?\nClick OK if yes.");
+  if (lowerOptions) {
+    var random2 = Math.floor(Math.random() * criteria.lowercaseLetters.length);
+    var randomLower = criteria.lowercaseLetters[random2];
+  }
+
   var numberOptions = window.confirm("Include numbers?\nClick OK if yes.");
+  if (numberOptions) {
+    var random3 = Math.floor(Math.random() * criteria.numbers.length);
+    var randomNumbers = criteria.numbers[random3];
+  }
+
   var specialOptions = window.confirm("Include special characters?\nClick OK if yes.");
+  if (specialOptions) {
+    var random4 = Math.floor(Math.random() * criteria.specialCharacters.length);
+    var randomSymbols = criteria.specialCharacters[random4];
+  }
   
   // Create instance in which they don't confirm anything
-  if (!upperOptions && !lowerOptions && !numberOptions && !specialOptions) {
+  else if (!upperOptions && !lowerOptions && !numberOptions && !specialOptions) {
     window.alert("You do not meet any of the criteria. Please try again.");
     writePassword();
   }
